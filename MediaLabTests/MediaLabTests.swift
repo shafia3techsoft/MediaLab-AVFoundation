3ed5eb8f601c790c23f7c50030ccac564587c70c
shafia.3techsoft <shafia.3techsoft@gmail.com>
2025-08-08 09:27:00 +0500

    Initial Commit

---
+++ MediaLabTests/MediaLabTests.swift
@@ -0,0 +1,17 @@
+//
+//  MediaLabTests.swift
+//  MediaLabTests
+//
+//  Created by shafia on 08/08/2025.
+//
+
+import Testing
+@testable import MediaLab
+
+struct MediaLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
